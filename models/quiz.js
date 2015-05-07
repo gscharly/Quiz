1e1e67c0f3b2973100e3432d230ec6762552920d
Carlos Gomez <charlygs94@gmail.com>
2015-05-07 21:27:29 +0200

    Base de datos

---
+++ models/quiz.js
@@ -0,0 +1,9 @@
+//Definicion del modelo de Qui<
+
+module.exports = function(sequelize, DataTypes){
+	return sequelize.define('Quiz',
+			{ pregunta: DataTypes.STRING,
+			  respuesta: DataTypes.STRING,
+			}
+		);
+}
