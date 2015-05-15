5e6f9a21bce164a3f772a3824db1f7624e39a3e7
Carlos Gomez <charlygs94@gmail.com>
2015-05-15 17:51:14 +0200

    Validacion de entradas con then

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -84,7 +84,7 @@ exports.new = function(req,res){
 
 exports.create = function(req,res){
 	var quiz = models.Quiz.build(req.body.quiz);
-	quiz.validate().success(
+	quiz.validate().then(
 		function(err){
 			if(err){
 				res.render('quizes/new', {quiz: quiz, errors: err.errors});
